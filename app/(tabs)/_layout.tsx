e161d6f655684a5794ea10ad23f57662d70091dd
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2025-07-27 20:09:17 -0500

    i change the name of the tasks file

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,7 +27,7 @@ export default function TabLayout() {
         }),
       }}>
       <Tabs.Screen
-        name="tasks"
+        name="index"
         options={{
           title: 'Tasks',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="checklist.checked" color={color} />,
